51b03296cfdf4b32776fce36d398b6482a36f722
EtishaGarg <gargetisha@gmail.com>
2023-06-19 14:02:15 +0530

    Router component added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Layout from './components/Home';
+import Layout from './components/Work';
+import Layout from './components/About';
+import Layout from './components/Contact';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-
-
-    </div>
+    <Routes>
+      <Route path='/' element={<Layout/>}/>
+        <Route index element={<Home/>}/>
+        <Route index element={<Work/>}/>
+        <Route index element={<About/>}/>
+        <Route index element={<Contact/>}/>
+    </Routes>
   );
 }
 
