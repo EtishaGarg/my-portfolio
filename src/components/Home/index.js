4b15dba7cacad7af00f34df7ae977f041dfa7547
EtishaGarg <gargetisha@gmail.com>
2023-06-22 16:09:32 +0530

    Added watch more button

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -13,14 +13,14 @@ const Home = () => {
                 <div>
                     <p className='mt-10 text-4xl font-bold'>👋Hey there, I'm Etisha</p>
                     <p className='mt-8 mr-60 text-lg mb-8'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <Link to='/contact' className='bg-purple-300 px-3 py-2 text-2xl font-bold rounded shadow-xl hover:ring-2'>CONTACT ME</Link>
+                    <Link to='/contact' className='bg-purple-300 px-3 py-2 text-2xl font-bold rounded shadow-xl hover:ring-2 hover:text-white'>CONTACT ME</Link>
                 </div>
                 <img src={profile} alt='profile image' className="h-[380px] w-[450px]"/>
             </div>
 
             {/* Youtube Links */}
 
-            <div className='flex-col justify-center items-center py-7 px-10'>
+            <div className='flex-col justify-center items-center py-7 px-10 bg-gray-100'>
                 <p className='mt-14 text-4xl font-bold text-center'>Learn from my Youtube Videos</p>
                 <p className='mt-3 text-lg font-normal text-center'>Sharing what I know and learnt, with the community</p>
 
@@ -31,7 +31,7 @@ const Home = () => {
                             <img className='w-full h-50' src={yt1}/>
                             <a href="https://youtu.be/vmABD38bITY" target="_blank" className='text-2xl font-medium hover:font-bold'>What is Open Source?</a>
                             <p className='mt-2 mb-4 text-lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <Link className='bg-purple-300 px-3 py-2 text-2xl font-bold rounded shadow-xl hover:ring-2' to={{ pathname: "https://youtu.be/vmABD38bITY" }} target="_blank">Watch on Youtube</Link>
+                            <a className='bg-purple-300 px-3 py-2 text-2xl font-bold rounded shadow-xl hover:ring-2 hover:text-white' href="https://youtu.be/vmABD38bITY" target="_blank">Watch on Youtube</a>
                         </div>
                     </div>
 
@@ -40,7 +40,7 @@ const Home = () => {
                             <img className='w-full h-50' src={yt1}/>
                             <a href="https://youtu.be/vmABD38bITY" target="_blank" className='text-2xl font-medium hover:font-bold'>What is Open Source?</a>
                             <p className='mt-2 mb-4 text-lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <Link className='bg-purple-300 px-3 py-2 text-2xl font-bold rounded shadow-xl hover:ring-2' to={{ pathname: "https://youtu.be/vmABD38bITY" }} target="_blank">Watch on Youtube</Link>
+                            <a className='bg-purple-300 px-3 py-2 text-2xl font-bold rounded shadow-xl hover:ring-2 hover:text-white' href="https://youtu.be/vmABD38bITY" target="_blank">Watch on Youtube</a>
                         </div>
                     </div>
 
@@ -49,7 +49,7 @@ const Home = () => {
                             <img className='w-full h-50' src={yt1}/>
                             <a href="https://youtu.be/vmABD38bITY" target="_blank" className='text-2xl font-medium hover:font-bold'>What is Open Source?</a>
                             <p className='mt-2 mb-4 text-lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <Link className='bg-purple-300 px-3 py-2 text-2xl font-bold rounded shadow-xl hover:ring-2' to={{ pathname: "https://youtu.be/vmABD38bITY" }} target="_blank">Watch on Youtube</Link>
+                            <a className='bg-purple-300 px-3 py-2 text-2xl font-bold rounded shadow-xl hover:ring-2 hover:text-white' href="https://youtu.be/vmABD38bITY" target="_blank">Watch on Youtube</a>
                         </div>
                     </div>
 
@@ -58,7 +58,7 @@ const Home = () => {
                             <img className='w-full h-50' src={yt1}/>
                             <a href="https://youtu.be/vmABD38bITY" target="_blank" className='text-2xl font-medium hover:font-bold'>What is Open Source?</a>
                             <p className='mt-2 mb-4 text-lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <Link className='bg-purple-300 px-3 py-2 text-2xl font-bold rounded shadow-xl hover:ring-2' to={{ pathname: "https://youtu.be/vmABD38bITY" }} target="_blank">Watch on Youtube</Link>
+                            <a className='bg-purple-300 px-3 py-2 text-2xl font-bold rounded shadow-xl hover:ring-2 hover:text-white' href="https://youtu.be/vmABD38bITY" target="_blank">Watch on Youtube</a>
                         </div>
                     </div>
 
@@ -67,7 +67,7 @@ const Home = () => {
                             <img className='w-full h-50' src={yt1}/>
                             <a href="https://youtu.be/vmABD38bITY" target="_blank" className='text-2xl font-medium hover:font-bold'>What is Open Source?</a>
                             <p className='mt-2 mb-4 text-lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <Link className='bg-purple-300 px-3 py-2 text-2xl font-bold rounded shadow-xl hover:ring-2' to={{ pathname: "https://youtu.be/vmABD38bITY" }} target="_blank">Watch on Youtube</Link>
+                            <a className='bg-purple-300 px-3 py-2 text-2xl font-bold rounded shadow-xl hover:ring-2 hover:text-white' href="https://youtu.be/vmABD38bITY" target="_blank">Watch on Youtube</a>
                         </div>
                     </div>
 
@@ -76,9 +76,12 @@ const Home = () => {
                             <img className='w-full h-50' src={yt1}/>
                             <a href="https://youtu.be/vmABD38bITY" target="_blank" className='text-2xl font-medium hover:font-bold'>What is Open Source?</a>
                             <p className='mt-2 mb-4 text-lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <Link className='bg-purple-300 px-3 py-2 text-2xl font-bold rounded shadow-xl hover:ring-2' to={{ pathname: "https://youtu.be/vmABD38bITY" }} target="_blank">Watch on Youtube</Link>
+                            <a className='bg-purple-300 px-3 py-2 text-2xl font-bold rounded shadow-xl hover:ring-2 hover:text-white' href="https://youtu.be/vmABD38bITY" target="_blank">Watch on Youtube</a>
                         </div>
                     </div>
+                </div>
+                <div className='grid mt-6'>
+                    <a className='bg-purple-300 font-bold text-2xl px-3 py-2 m-auto rounded shadow-xl hover:ring-2 hover:text-white' href="https://www.youtube.com/channel/UCNliY-e1-7VNtuWgrSib1UQ" target="_blank">Click to watch more! &gt;&gt;&gt;</a>
                 </div>
             </div>
             {/* Developer community */}
