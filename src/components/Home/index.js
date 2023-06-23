4a9254ff5f8ff5e6c25b7bcfa968248c86e8a0a6
EtishaGarg <gargetisha@gmail.com>
2023-06-23 15:26:17 +0530

    Issue not loading in reusable component fixed

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -38,67 +38,16 @@ const Home = () => {
                 <p className='mt-3 text-lg font-normal text-center'>Sharing what I know and learnt, with the community</p>
 
                 <div className='grid grid-cols-3 gap-8 mt-6'>
-
                     {yt_cards}
-                    {/* <div className='h-[450px] p-10 bg-purple-300 rounded-xl'>
-                        <div className='flex-col text-center h-[380px] bg-white shadow-xl rounded-xl overflow-hidden'>
-                            <img className='w-full h-50' src={yt1}/>
-                            <a href="https://youtu.be/vmABD38bITY" target="_blank" className='text-2xl font-medium hover:font-bold'>What is Open Source?</a>
-                            <p className='mt-2 mb-4 text-lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <a className='bg-purple-300 px-3 py-2 text-2xl font-bold rounded shadow-xl hover:ring-2 hover:text-white' href="https://youtu.be/vmABD38bITY" target="_blank">Watch on Youtube</a>
-                        </div>
-                    </div>
-
-                    <div className='h-[450px] p-10 bg-purple-300 rounded-xl'>
-                        <div className='flex-col text-center h-[380px] bg-white shadow-xl rounded-xl overflow-hidden'>
-                            <img className='w-full h-50' src={yt1}/>
-                            <a href="https://youtu.be/vmABD38bITY" target="_blank" className='text-2xl font-medium hover:font-bold'>What is Open Source?</a>
-                            <p className='mt-2 mb-4 text-lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <a className='bg-purple-300 px-3 py-2 text-2xl font-bold rounded shadow-xl hover:ring-2 hover:text-white' href="https://youtu.be/vmABD38bITY" target="_blank">Watch on Youtube</a>
-                        </div>
-                    </div>
-
-                    <div className='h-[450px] p-10 bg-purple-300 rounded-xl'>
-                        <div className='flex-col text-center h-[380px] bg-white shadow-xl rounded-xl overflow-hidden'>
-                            <img className='w-full h-50' src={yt1}/>
-                            <a href="https://youtu.be/vmABD38bITY" target="_blank" className='text-2xl font-medium hover:font-bold'>What is Open Source?</a>
-                            <p className='mt-2 mb-4 text-lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <a className='bg-purple-300 px-3 py-2 text-2xl font-bold rounded shadow-xl hover:ring-2 hover:text-white' href="https://youtu.be/vmABD38bITY" target="_blank">Watch on Youtube</a>
-                        </div>
-                    </div>
-
-                    <div className='h-[450px] p-10 bg-purple-300 rounded-xl'>
-                        <div className='flex-col text-center h-[380px] bg-white shadow-xl rounded-xl overflow-hidden'>
-                            <img className='w-full h-50' src={yt1}/>
-                            <a href="https://youtu.be/vmABD38bITY" target="_blank" className='text-2xl font-medium hover:font-bold'>What is Open Source?</a>
-                            <p className='mt-2 mb-4 text-lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <a className='bg-purple-300 px-3 py-2 text-2xl font-bold rounded shadow-xl hover:ring-2 hover:text-white' href="https://youtu.be/vmABD38bITY" target="_blank">Watch on Youtube</a>
-                        </div>
-                    </div>
-
-                    <div className='h-[450px] p-10 bg-purple-300 rounded-xl'>
-                        <div className='flex-col text-center h-[380px] bg-white shadow-xl rounded-xl overflow-hidden'>
-                            <img className='w-full h-50' src={yt1}/>
-                            <a href="https://youtu.be/vmABD38bITY" target="_blank" className='text-2xl font-medium hover:font-bold'>What is Open Source?</a>
-                            <p className='mt-2 mb-4 text-lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <a className='bg-purple-300 px-3 py-2 text-2xl font-bold rounded shadow-xl hover:ring-2 hover:text-white' href="https://youtu.be/vmABD38bITY" target="_blank">Watch on Youtube</a>
-                        </div>
-                    </div>
-
-                    <div className='h-[450px] p-10 bg-purple-300 rounded-xl'>
-                        <div className='flex-col text-center h-[380px] bg-white shadow-xl rounded-xl overflow-hidden'>
-                            <img className='w-full h-50' src={yt1}/>
-                            <a href="https://youtu.be/vmABD38bITY" target="_blank" className='text-2xl font-medium hover:font-bold'>What is Open Source?</a>
-                            <p className='mt-2 mb-4 text-lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <a className='bg-purple-300 px-3 py-2 text-2xl font-bold rounded shadow-xl hover:ring-2 hover:text-white' href="https://youtu.be/vmABD38bITY" target="_blank">Watch on Youtube</a>
-                        </div>
-                    </div> */}
                 </div>
+
                 <div className='grid mt-6'>
                     <a className='bg-purple-300 font-bold text-2xl px-3 py-2 m-auto rounded shadow-xl hover:ring-2 hover:text-white' href="https://www.youtube.com/channel/UCNliY-e1-7VNtuWgrSib1UQ" target="_blank">Click to watch more! &gt;&gt;&gt;</a>
                 </div>
             </div>
+
             {/* Developer community */}
+            
 
 
             
