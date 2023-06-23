93d8408451032af9ed7d42bd81e871a693339607
EtishaGarg <gargetisha@gmail.com>
2023-06-23 13:49:46 +0530

    Created reusable component for Youtube grid

---
+++ src/data/YT_data.js
@@ -0,0 +1,66 @@
+export default [
+    {
+        id: 1,
+        link: "https://youtu.be/vmABD38bITY",
+        title: "What is Open Source?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        thumbnail: "../assets/images/yt1.png"
+    },
+    {
+        id: 2,
+        link: "https://youtu.be/vmABD38bITY",
+        title: "Twitter Like Animation?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        thumbnail: "../assets/images/yt1.png"
+    },
+    {
+        id: 3,
+        link: "https://youtu.be/vmABD38bITY",
+        title: "What is Open Source?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        thumbnail: "../assets/images/yt1.png"
+    },
+    {
+        id: 4,
+        link: "https://youtu.be/vmABD38bITY",
+        title: "What is Open Source?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        thumbnail: "./assets/images/yt1.png"
+    },
+    {
+        id: 5,
+        link: "https://youtu.be/vmABD38bITY",
+        title: "What is Open Source?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        thumbnail: "../assets/images/yt1.png"
+    },
+    {
+        id: 6,
+        link: "https://youtu.be/vmABD38bITY",
+        title: "What is Open Source?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        thumbnail: "../assets/images/yt1.png"
+    },
+    {
+        id: 7,
+        link: "https://youtu.be/vmABD38bITY",
+        title: "What is Open Source?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        thumbnail: "../assets/images/yt1.png"
+    },
+    {
+        id: 8,
+        link: "https://youtu.be/vmABD38bITY",
+        title: "What is Open Source?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        thumbnail: "../assets/images/yt1.png"
+    },
+    {
+        id: 9,
+        link: "https://youtu.be/vmABD38bITY",
+        title: "What is Open Source?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        thumbnail: "../assets/images/yt1.png"
+    }
+
+]
