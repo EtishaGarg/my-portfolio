8fbb3328b6738350df6d40f17b1c51670282db00
EtishaGarg <gargetisha@gmail.com>
2023-06-24 23:08:59 +0530

    Data file for community event added

---
+++ src/data/Dev_data.js
@@ -0,0 +1,19 @@
+import img1 from "../assets/img1.png";
+
+export default [
+    {
+        id: 1,
+        description: "> Lorem ipsum dolor sit amet, consectetur adipiscing elite. Lorem ipsum dolor sit amet, consectetur adipiscing elite.",
+        image: img1
+    },
+    {
+        id: 2,
+        description: "> Lorem ipsum dolor sit amet, consectetur adipiscing elite. Lorem ipsum dolor sit amet, consectetur adipiscing elite.",
+        image: img1
+    },
+    {
+        id: 3,
+        description: "> Lorem ipsum dolor sit amet, consectetur adipiscing elite. Lorem ipsum dolor sit amet, consectetur adipiscing elite.",
+        image: img1
+    }
+]
