86d0493bb893f15b2cb75633e3d85644f1ef8bc0
EtishaGarg <gargetisha@gmail.com>
2023-07-01 18:08:28 +0530

    Experience and story section added

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,6 @@
 import './index.scss'
 import profile1 from '../../assets/profile_1.png'
+import logo1 from '../../assets/logo1.png'
 
 const About = () => {
     return(
@@ -16,6 +17,50 @@ const About = () => {
                 </div>
                 <img src={profile1} alt="profile"/>
             </div>
+
+
+            {/* Experiences */}
+
+            <div className='flex flex-col py-7 px-10'>
+                <h1 className='font-bold text-3xl'>My Experiences</h1>
+                <div className='flex items-center justify-center mt-10'>
+                    <img src={logo1} alt="logo" className='h-50 w-30 ml-12'></img>
+                    <p className='ml-12 text-lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                </div>
+                <div className='flex items-center justify-center mt-8'>
+                    <img src={logo1} alt="logo" className='h-50 w-30 ml-12'></img>
+                    <p className='ml-12 text-lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                </div>
+                <div className='flex items-center justify-center mt-8'>
+                    <img src={logo1} alt="logo" className='h-50 w-30 ml-12'></img>
+                    <p className='ml-12 text-lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                </div>
+            </div>
+
+            {/* Experiences */}
+
+            <div className='flex flex-col py-7 px-10'>
+                <h1 className='font-bold text-3xl'>My Story</h1>
+                <div className='flex flex-col gap-6 items-center justify-center text-left mt-10'>
+                    <div>
+                        <h1 className='ml-12 text-2xl font-bold'>Phase-1</h1>
+                        <p className='ml-12 text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    </div>
+                    <div>
+                        <h1 className='ml-12 text-2xl font-bold'>Phase-2</h1>
+                        <p className='ml-12 text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    </div>
+                    <div>
+                        <h1 className='ml-12 text-2xl font-bold'>Phase-3</h1>
+                        <p className='ml-12 text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    </div>
+                    <div>
+                        <h1 className='ml-12 text-2xl font-bold'>Phase-4</h1>
+                        <p className='ml-12 text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    </div>
+                </div>
+            </div>
+
 
         </div>
     )
